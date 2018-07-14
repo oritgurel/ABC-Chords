4f40152bed95d54eb719cf1a5808570a7d735bb4
OGIT <oritgurel@gmail.com>
2018-07-14 22:35:27 +0300

    create sheet activity layout

---
+++ app/src/main/java/com/oritmalki/mymusicapp2/ui/mainscreen/CreateNewSheetActivity.java
@@ -0,0 +1,15 @@
+package com.oritmalki.mymusicapp2.ui.mainscreen;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.oritmalki.mymusicapp2.R;
+
+public class CreateNewSheetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_sheet);
+    }
+}
